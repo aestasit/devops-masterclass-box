861417f9cfdb446c104abbae8a04d3fbf54e3806
Andrey Adamovich <andrey@aestasit.com>
2016-09-07 21:38:28 +0300

    updated compose version

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -20,7 +20,7 @@ apt-get install -y -q docker-engine
 usermod -aG docker vagrant
 
 echo "*** Installing Docker Compose ***"
-su -c "curl -sSL https://github.com/docker/compose/releases/download/1.4.0/docker-compose-Linux-x86_64 > /usr/local/bin/docker-compose"
+su -c "curl -sSL https://github.com/docker/compose/releases/download/1.8.0/docker-compose-Linux-x86_64 > /usr/local/bin/docker-compose"
 chmod +x /usr/local/bin/docker-compose
 
 echo "*** Install utilities ***"
