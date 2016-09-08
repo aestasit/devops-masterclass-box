8ffd8008df3d92888814ed26f1627ca0558e71ab
Andrey Adamovich <andrey@aestasit.com>
2016-09-08 23:42:43 +0300

    added build for aws

--- build-devops-ubuntu-14.04.bat
+++ build-devops-ubuntu-14.04.bat
@@ -1,2 +1,3 @@
 @echo off
-@packer build -force boxes/devops-ubuntu-14.04.json
+@packer build -force boxes/devops-ubuntu-14.04-virtualbox.json
+@packer build -force boxes/devops-ubuntu-14.04-aws.json
