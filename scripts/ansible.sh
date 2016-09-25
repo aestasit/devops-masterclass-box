16d424d4b32a6bcf78074acf44149f43679626f5
Andrey Adamovich <andrey@aestasit.com>
2016-09-26 00:56:41 +0300

    added ansible module installation

--- scripts/ansible.sh
+++ scripts/ansible.sh
@@ -4,5 +4,9 @@ echo "*** Installing Ansible ***"
 apt-get -y install --reinstall ca-certificates
 apt-get -y install ansible
 
+echo "*** Installing Ansible roles ***"
+ansible-galaxy install geerlingguy.apache
+ansible-galaxy install geerlingguy.mysql
+
 echo "*** Configuring Ansible to run on localhost ***"
 echo 'localhost ansible_connection=local' > /etc/ansible/hosts
