bd049cbe7c9732c67ee4a0408d16b2450431e858
Andrey Adamovich <andrey@aestasit.com>
2017-07-23 19:34:52 +0300

    fixed varant cloud deployment

--- build-devops-ubuntu-16.04-virtualbox.cmd
+++ build-devops-ubuntu-16.04-virtualbox.cmd
@@ -2,5 +2,5 @@
 @set PACKER_LOG=1
 @set PACKER_LOG_PATH=devops-ubuntu-16.04-virtualbox.log
 @del /s /q output\*.box
-@packer build -force boxes/devops-ubuntu-16.04-virtualbox.json
+@packer build -var-file=secrets.json -force boxes/devops-ubuntu-16.04-virtualbox.json
 @pause
