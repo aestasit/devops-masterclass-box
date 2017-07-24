9f2f63806b9256b7e32585c62e02b40519e0066b
Andrey Adamovich <andrey@aestasit.com>
2017-07-24 14:13:20 +0300

    un-register before register

--- register-devops-ubuntu-16.04.cmd
+++ register-devops-ubuntu-16.04.cmd
@@ -1,3 +1,3 @@
 @echo off
-@rem call vagrant box remove --all --force aestasit/devops-ubuntu-16.04 
+@call vagrant box remove --all --force aestasit/devops-ubuntu-16.04 
 @call vagrant box add --force aestasit/devops-ubuntu-16.04 output/devops-ubuntu-16-04-x64-virtualbox-*.box 
