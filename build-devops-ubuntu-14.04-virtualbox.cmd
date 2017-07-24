a074cf9a64c1ef856544231590e71feb3f700c31
Andrey Adamovich <andrey@aestasit.com>
2017-07-24 09:51:04 +0300

    removed vagrant cloud

--- build-devops-ubuntu-14.04-virtualbox.cmd
+++ build-devops-ubuntu-14.04-virtualbox.cmd
@@ -2,5 +2,5 @@
 @set PACKER_LOG=1
 @set PACKER_LOG_PATH=devops-ubuntu-14.04-virtualbox.log
 @del /s /q output\*.box
-@packer build -var-file=secrets.json -force boxes/devops-ubuntu-14.04-virtualbox.json
+@packer build -force boxes/devops-ubuntu-14.04-virtualbox.json
 @pause              
