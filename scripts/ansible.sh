5f78db0528d8bdf7e14fc88e3d2e225e3906abe9
Andrey Adamovich <andrey@aestasit.com>
2017-08-02 23:18:39 +0300

    install docker-py to make ansible happy

--- scripts/ansible.sh
+++ scripts/ansible.sh
@@ -4,6 +4,7 @@ echo "*** Installing Ansible ***"
 apt-add-repository -y ppa:ansible/ansible
 apt-get -y -qq update -o=Dpkg::Use-Pty=0 
 apt-get -y -qq install -o=Dpkg::Use-Pty=0 ansible
+pip install docker-py
 
 echo "*** Configuring Ansible to run on localhost ***"
 echo 'localhost ansible_connection=local' > /etc/ansible/hosts
