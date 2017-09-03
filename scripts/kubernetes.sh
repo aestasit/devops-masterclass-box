07e8f8cacb6996f07f4604ebcb350acb963133a5
Andrey Adamovich <andrey@aestasit.com>
2017-09-03 19:49:47 +0300

    added minikube

--- scripts/kubernetes.sh
+++ scripts/kubernetes.sh
@@ -4,4 +4,10 @@ echo "*** Installing kubectl ***"
 apt-get -y -qq install -o=Dpkg::Use-Pty=0 bridge-utils
 curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
 chmod +x ./kubectl
-sudo mv ./kubectl /usr/local/bin/kubectl
+mv ./kubectl /usr/local/bin/kubectl
+
+echo "*** Installing minikube ***"
+curl -Lo minikube https://storage.googleapis.com/minikube/releases/v0.21.0/minikube-linux-amd64 
+chmod +x minikube 
+mv minikube /usr/local/bin/
+
