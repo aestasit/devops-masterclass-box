2deb574b69cda4e889a23db8c845a597c4695343
Andrey Adamovich <andrey@aestasit.com>
2018-12-20 10:10:07 +0100

    updated version of ubuntu iso

--- register-devops-ubuntu-16.04.cmd
+++ register-devops-ubuntu-16.04.cmd
@@ -1,9 +1,9 @@
 @echo off
 
-set VERSION=2018-05-12
+set VERSION=2018-12-18
 
-call vagrant box remove --provider vmware_desktop --force aestasit/devops-ubuntu-16.04 
-call vagrant box add --provider vmware_desktop --force aestasit/devops-ubuntu-16.04 file://.output/devops-ubuntu-16-04-x64-vmware-%VERSION%.box 
+rem call vagrant box remove --provider vmware_desktop --force aestasit/devops-ubuntu-16.04 
+rem call vagrant box add --provider vmware_desktop --force aestasit/devops-ubuntu-16.04 file://.output/devops-ubuntu-16-04-x64-vmware-%VERSION%.box 
 
 rem call vagrant box remove --provider hyperv --force aestasit/devops-ubuntu-16.04 
 rem call vagrant box add --provider hyperv --force aestasit/devops-ubuntu-16.04 file://.output/devops-ubuntu-16-04-x64-hyperv-%VERSION%.box 
--- tests/test.cmd
+++ tests/test.cmd
@@ -1,12 +1,12 @@
 @echo off
 
-@call gradlew -b test.gradle -Pinfra=aws createServer || goto :error
-@call gradlew -b test.gradle -Pinfra=aws uploadTests runTests || goto :error
-@call gradlew -b test.gradle -Pinfra=aws destroyServer || goto :error
+@rem call gradlew -b test.gradle -Pinfra=aws createServer || goto :error
+@rem call gradlew -b test.gradle -Pinfra=aws uploadTests runTests || goto :error
+@rem call gradlew -b test.gradle -Pinfra=aws destroyServer || goto :error
 
-@rem call gradlew -b test.gradle -Pinfra=vagrant createServer || goto :error
-@rem call gradlew -b test.gradle -Pinfra=vagrant uploadTests runTests || goto :error
-@rem call gradlew -b test.gradle -Pinfra=vagrant destroyServer || goto :error
+@call gradlew -b test.gradle -Pinfra=vagrant createServer || goto :error
+@call gradlew -b test.gradle -Pinfra=vagrant uploadTests runTests || goto :error
+@call gradlew -b test.gradle -Pinfra=vagrant destroyServer || goto :error
 
 @goto :EOF
 
