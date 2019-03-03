2aea32e2f2691c45ebbc4285613e0784c9834ddd
Andrey Adamovich <andrey@aestasit.com>
2019-03-04 00:28:03 +0200

    gcp and goss

--- scripts/ubuntu/clouds/gcp-cli.sh
+++ scripts/ubuntu/clouds/gcp-cli.sh
@@ -6,7 +6,6 @@ echo "deb http://packages.cloud.google.com/apt $CLOUD_SDK_REPO main" | sudo tee 
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 apt-get -y -qq update -o=Dpkg::Use-Pty=0
 apt-get -y -qq install -o=Dpkg::Use-Pty=0 google-cloud-sdk
-gcloud --quiet components update
 
 echo "*** Checking GCP CLI ***"
 gcloud version
